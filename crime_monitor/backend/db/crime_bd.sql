c986dc523f15cfd21bd59d3b5d933e80224e932f
RafaelEduSenac <rafael.lopes@edu.pe.senac.br>
2025-10-08 19:27:35 -0300

    Adicionando arquivo de criação do banco de dados PostgreSQL

---
+++ crime_monitor/backend/db/crime_bd.sql
@@ -0,0 +1,45 @@
+CREATE DATABASE crimes;
+
+\c crimes
+
+CREATE TABLE IF NOT EXISTS dados_previstos
+(
+    prev_id bigserial NOT NULL,
+    cisp smallint NOT NULL,
+    mcirc bigint NOT NULL,
+    mes smallint NOT NULL,
+    ano integer NOT NULL,
+    tentat_hom integer,
+    estupro integer,
+    lesao_corp_culposa integer,
+    roubo_veiculo integer,
+    estelionato integer,
+    apreensao_drogas integer,
+    trafico_drogas integer,
+    apf integer,
+    pessoas_desaparecidas integer,
+    encontro_cadaver integer,
+    registro_ocorrencias integer,
+    CONSTRAINT dados_previstos_pkey PRIMARY KEY (prev_id)
+);
+
+CREATE TABLE IF NOT EXISTS dados_reais
+(
+    real_id bigserial NOT NULL,
+    cisp smallint NOT NULL,
+    mcirc bigint NOT NULL,
+    mes smallint NOT NULL,
+    ano integer NOT NULL,
+    tentat_hom integer,
+    estupro integer,
+    lesao_corp_culposa integer,
+    roubo_veiculo integer,
+    estelionato integer,
+    apreensao_drogas integer,
+    trafico_drogas integer,
+    apf integer,
+    pessoas_desaparecidas integer,
+    encontro_cadaver integer,
+    registro_ocorrencias integer,
+    CONSTRAINT dados_reais_pkey PRIMARY KEY (real_id)
+);
